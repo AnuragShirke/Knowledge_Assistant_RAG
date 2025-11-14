cee7df2acb04ba464acdae0a93dd5121545445c1
AnuragShirke <anuragshirke011@gmail.com>
2025-11-14 22:37:13 +0530

    Minor Changes

--- src/core/processing.py
+++ src/core/processing.py
@@ -1,5 +1,5 @@
 import fitz  # PyMuPDF
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitters import RecursiveCharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 import docx # Added for docx parsing
 
